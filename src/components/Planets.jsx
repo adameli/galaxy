a73dd68e002a48b8c65edd2de0c73d8468947d14
Adam Eliasson <adam.eliasson.swe@gmail.com>
2024-11-21 18:45:32 +0100

    Making stuff move

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -1,5 +1,8 @@
-import { useFrame } from "@react-three/fiber"
-import { useRef } from "react"
+import { useFrame, useThree } from "@react-three/fiber"
+import { useRef, useState } from "react"
+import { useControls, button } from "leva"
+import * as THREE from 'three'
+import { gsap } from "gsap"
 
 // createPlanet(0.2, 1, 0.025, './textures/matcaps/mercury.jpg') // Mercury
 // createPlanet(0.3, 3,  0.018, './textures/matcaps/venus.jpg'),  // Venus
@@ -10,6 +13,7 @@ import { useRef } from "react"
 // createPlanet(0.8, 16,  0.004,'./textures/matcaps/uranus.jpg'),  // Uranus
 // createPlanet(0.7, 18,  0.003, './textures/matcaps/neptune.jpg'),  // Neptune
 export default function Planets() {
+
     // Define planets as an array of configurations
     const planetsConfig = [
         { size: 0.2, distanceFromSun: 2, orbitSpeed: 0.025, color: 'orange' }, //Mercury
@@ -22,10 +26,59 @@ export default function Planets() {
         { size: 0.7, distanceFromSun: 18, orbitSpeed: 0.003, color: 'mediumPurple' }, //Neptune
     ]
 
+    let isPausePlay = false
+
     // Create refs for all planets
     const planetsRefs = planetsConfig.map(() => useRef())
 
-    console.log(planetsRefs);
+    //Camera
+    const camera = useThree(state => state.camera)
+
+    // Sun ref
+    const sun = useRef()
+
+    const [targetPosition, setTargetPosition] = useState(null)
+
+    useFrame(() => {
+        if (targetPosition) {
+            // Interpolate camera position toward the target, change the z value to get the camera further och closer to the target
+            camera.position.lerp(targetPosition.clone().add(new THREE.Vector3(0, 0, 4)), 0.03); // 0.03 the speed of the camera to move to the object
+
+            // Adjust camera to look at the target position
+            camera.lookAt(targetPosition);
+
+            // Stop animating once the camera is close enough to the target,  change the z value to get the camera further och closer to the target
+            if (camera.position.distanceTo(targetPosition.clone().add(new THREE.Vector3(0, 0, 4))) < 0.5) {
+                // setTargetPosition(null); // Clear target position to stop animation
+            }
+        }
+    });
+
+    function movePlanet(planetIndex) {
+
+        isPausePlay = true
+
+        const activePlanet = planetsRefs[4].current.position
+
+
+        gsap.to(planetsRefs[4].current.position, {
+            y: 6,
+            onComplete: () => {
+                setTargetPosition(planetsRefs[4].current.position.clone());
+            }
+        })
+
+    }
+
+    useControls({
+        MoveCamera: button((e) => {
+            setTargetPosition(sun.current.position.clone());
+        }),
+        Jupiter: button(() => {
+            // setTargetPosition(planetsRefs[4].current.position.clone());
+            movePlanet()
+        })
+    })
 
 
     // Update the orbit of each planet on every frame
@@ -50,7 +103,7 @@ export default function Planets() {
     return (
         <>
             {/* Sun */}
-            <mesh position={[0, 0, 0]}>
+            <mesh ref={sun} position={[0, 0, 0]}>
                 <sphereGeometry />
                 <meshStandardMaterial color="yellow" />
             </mesh>
